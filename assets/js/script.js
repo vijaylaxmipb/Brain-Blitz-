bcad70d87976ec9c80621f47c4d401358e8a02d3
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-03-21 17:46:05 +0000

    Add questions,options and answers to js file

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+//Question part
+
+const questionPart=[
+    {
+        question : 'What is the hardest natural substance on planet Earth?',
+        Option :['Gold','Diamond','Platinum','steel'],
+        answer : 'Diamond'
+
+    },
+    {
+        question : 'Which planet has 145 moons?',
+        Option : ['Saturn','Mars','Mercury','Jupiter'],
+        answer : 'Saturn'
+    },
+    {
+        question : 'In which country was the game of chess invented?',
+        Option :['India','China','Greece','USA'],
+        answer : 'India'
+
+    },
+    {
+        question : 'What was the first fruit to be eaten on the Moon?',
+        Option :['Grapes','A peach','A starfruit','Apple'],
+        answer : 'A peach'
+
+    },
+    {
+        question : 'What was the first fruit to be eaten on the Moon?',
+        Option :['Grapes','A peach','A starfruit','Apple'],
+        answer : 'A peach'
+
+    },
+    {
+        question : 'Which continent is home to the snow leopard?',
+        Option : ['North America','Asia','Africa','South America'],
+        answer : 'Asia'
+    },
+    {
+        question : 'What word appears on cold taps in Italy?',
+        Option : ['Freddo','Froggie','Bruno','Carlo'],
+        answer : 'Freddo'
+    },
+    {
+        question : 'Which country did Transylvania belong to from 11th century until 1918?',
+        Option : ['Germany','Romania','Britain','Hungary'],
+        answer : 'Hungary'
+    },
+    {
+        question : 'WHICH of these is NOT a recognised name for a young beaver?',
+        Option : ['Mouse','Kit','Pup','Kitten'],
+        answer : 'Mouse'
+    },
+
+
+
+
+
+]
